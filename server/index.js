a25fbff02687ffb310af5fcbe40e0a04c48263f8
Jason Melnikel <jmelnikel@gmail.com>
2021-06-26 11:15:07 -0400

    trying to deploy

--- server/index.js
+++ server/index.js
@@ -17,8 +17,10 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
 app.get("/user/:email", async (req, res) => {
   try {
     const { email } = req.params;
+    console.log("This is email going in for DB query", email)
     const userLoginData = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
     res.json(userLoginData.rows[0]);
+    console.log("This is userLoginData", userLoginData.rows[0])
   } catch (error) {
     throw new Error(error.message);
   }
