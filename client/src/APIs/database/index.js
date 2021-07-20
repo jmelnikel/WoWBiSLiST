55300b4b9684af313239135768ade02bde43d837
Jason Melnikel <jmelnikel@gmail.com>
2021-07-20 13:26:29 -0400

    everything works at this point. Try to deploy

--- client/src/APIs/database/index.js
+++ client/src/APIs/database/index.js
@@ -1,11 +1,12 @@
 /* eslint-disable space-before-function-paren */
 import axios from 'axios';
 
+const baseURL = "http://localhost:3001/api"
+
+
 export const getUserLoginData = async (email) => {
   const url = `/user/${email}`
-  const getUserLoginDataAxios = axios.create({
-    baseURL: "http://localhost:3001/api",
-  });
+  const getUserLoginDataAxios = axios.create({ baseURL });
 
   const response = getUserLoginDataAxios.get(url);
   return response;
@@ -24,54 +25,48 @@ export const getUserLoginData = async (email) => {
 
 export const clearArmorTable = async () => {
   const url = process.env.REACT_APP_CLEAR_ARMOR_TABLE_URL
-  const clearArmorTableAxios = axios.create({
-    baseURL: "http://localhost:3001/api",
-  });
+  const clearArmorTableAxios = axios.create({ baseURL });
+
   clearArmorTableAxios.post(url)
   console.log("Table: armor - Cleared and initialized")
 }
 
 export const writeDetailDataArmorTable = async (data) => {
   const url = process.env.REACT_APP_WRITE_DETAIL_DATA_ARMOR_TABLE_URL
-  const writeDetailDataArmorTableAxios = axios.create({
-    baseURL: "http://localhost:3001/api",
-  });
+  const writeDetailDataArmorTableAxios = axios.create({ baseURL });
+
   await writeDetailDataArmorTableAxios.post(url, data)
   console.log("Table: armor - Data sent to be written")
 }
 
 export const deleteDuplicateRowsArmorTable = async () => {
   const url = process.env.REACT_APP_DELETE_DUPLICATE_ROWS_ARMOR_TABLE_URL
-  const deleteDuplicateRowsArmorTableAxios = axios.create({
-    baseURL: "http://localhost:3001/api",
-  });
+  const deleteDuplicateRowsArmorTableAxios = axios.create({ baseURL });
+
   deleteDuplicateRowsArmorTableAxios.post(url)
   console.log("Table: armor - Duplicate data deleted")
 }
 
 export const clearWeaponTable = async () => {
   const url = process.env.REACT_APP_CLEAR_WEAPON_TABLE_URL
-  const clearWeaponTableAxios = axios.create({
-    baseURL: "http://localhost:3001/api",
-  });
+  const clearWeaponTableAxios = axios.create({ baseURL });
+
   clearWeaponTableAxios.post(url)
   console.log("Table: weapon - Cleared and initialized")
 }
 
 export const writeDetailDataWeaponTable = async (data) => {
   const url = process.env.REACT_APP_WRITE_DETAIL_DATA_WEAPON_TABLE_URL
-  const writeDetailDataWeaponTableAxios = axios.create({
-    baseURL: "http://localhost:3001/api",
-  });
+  const writeDetailDataWeaponTableAxios = axios.create({ baseURL });
+
   await writeDetailDataWeaponTableAxios.post(url, data)
   console.log("Table: weapon - Data sent to be written")
 }
 
 export const deleteDuplicateRowsWeaponTable = async (data) => {
   const url = process.env.REACT_APP_DELETE_DUPLICATE_ROWS_WEAPON_TABLE_URL
-  const deleteDuplicateRowsWeaponTableAxios = axios.create({
-    baseURL: "http://localhost:3001/api",
-  });
+  const deleteDuplicateRowsWeaponTableAxios = axios.create({ baseURL });
+
   deleteDuplicateRowsWeaponTableAxios.post(url)
   console.log("Table: weapon - Duplicate data deleted")
 }
@@ -95,16 +90,14 @@ export const deleteDuplicateRowsWeaponTable = async (data) => {
 
 export const getArmorItems = async () => {
   const url = "/armor"
-  const getArmorItemsAxios = axios.create({
-    baseURL: "http://localhost:3001/api",
-  });
+  const getArmorItemsAxios = axios.create({ baseURL });
+
   return await getArmorItemsAxios.get(url)
 }
 
 export const getWeaponItems = async () => {
   const url = "/weapon"
-  const getWeaponItemsAxios = axios.create({
-    baseURL: "http://localhost:3001/api",
-  });
+  const getWeaponItemsAxios = axios.create({ baseURL });
+
   return await getWeaponItemsAxios.get(url)
 }
